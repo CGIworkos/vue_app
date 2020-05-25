a265cb0f1e925d86c40e54cba42a7d0191c42dfe
CGIworkos <archivizer.workos@gmail.com>
2020-05-25 21:26:17 +0300

    FireBase API

--- src/main.js
+++ src/main.js
@@ -8,14 +8,36 @@ import messagePlugin from '@/utils/message.plugin.js'
 import './registerServiceWorker'
 import'materialize-css/dist/js/materialize.min.js'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: "AIzaSyB7R-GBlTn5BZU5ba7MS45dYgKbwBryrGo",
+  authDomain: "vue-crm-bd.firebaseapp.com",
+  databaseURL: "https://vue-crm-bd.firebaseio.com",
+  projectId: "vue-crm-bd",
+  storageBucket: "vue-crm-bd.appspot.com",
+  messagingSenderId: "690936104433",
+  appId: "1:690936104433:web:e478b75fd3bcfd6be6f050"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(()  => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }  
+})
+
+
