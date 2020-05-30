627f4fd884b509bed3457d9169723890e2510992
CGIworkos <archivizer.workos@gmail.com>
2020-05-30 23:51:29 +0300

    Complete record page

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -63,7 +63,7 @@
             id="description"
             type="text"
             v-model="description"
-            :class="{invalid: $v.description.$dirty && !$v.description.minValue}"
+            :class="{invalid: $v.description.$dirty && !$v.description.required}"
 
         >
         <label for="description">Описание</label>
@@ -85,6 +85,7 @@
 
 <script>
 import {required, minValue} from 'vuelidate/lib/validators'
+import {mapGetters} from 'vuex'
 
 export default {
   name: 'record',
@@ -111,16 +112,48 @@ export default {
 
     setTimeout(() => {
       this.select = M.FormSelect.init(this.$refs.select)
+      M.updateTextFields()
     }, 0)
   },
+  computed: {
+    ...mapGetters(['info']),
+    canCreateRecord() {
+      if(this.type === 'income') {
+        return true
+      }
+
+      return this.info.bill >= this.amount
+    }
+  },
   methods: {
-    handleSubmit() {
+    async handleSubmit() {
         if (this.$v.$invalid) {
         this.$v.$touch()
         return
       }
+    if (this.canCreateRecord) {
+      try {
+      await this.$store.dispatch('createRecord', {
+        categoryId: this.category,
+        amount: this.amount,
+        description: this.description,
+        date: new Date().toJSON()
+      })
+      const bill = this.type === 'income'
+        ? this.info.bill + this.amount
+        : this.info.bill - this.amount
 
+        await this.$store.dispatch('updateInfo', {bill})
+        this.$message('Запись успешно создана')
+        this.$v.$reset()
+        this.amount = 1
+        this.description = ''
+        
+      } catch(e) {}
+} else {
+      this.$message(`Недостаточно средств на счёте (${this.amount - this.info.bill})`)
     }
+  }
   },
   destroyed() {
     if (this.select && this.select.destroy) {
